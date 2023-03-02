692bc058436427522cb019016134b23e814dd167
barreirojm <barreirojuanmanuel@gmail.com>
2023-03-02 19:54:41 -0300

    card detalles dinamica find + urlparams

---
+++ assets/js/details.js
@@ -0,0 +1,33 @@
+const queryString = location.search;
+
+const params = new URLSearchParams(queryString);
+
+const id = params.get("id");
+
+const cardEvento = data.events.find((event) => event._id == id);
+
+console.log(cardEvento);
+
+const card = document.getElementById("card-detail");
+
+card.innerHTML = `
+        <div class="container-card d-flex justify-content-center">
+                        <div class="card mb-3 mt-5 " style="max-width: 1440px;">
+                            <div class="row no-gutters">
+                                <div class="col-md-6">
+                                    <img src="${cardEvento.image}" class="card-img img-fit-detail" alt="...">
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="card-body">
+                                        <h5 class="card-title">${cardEvento.name}</h5>
+                                        <p class="card-text">${cardEvento.description}</p>
+                                        <p class="card-text">Place: ${cardEvento.place}</p>
+                                        <p class="card-text">Price: $${cardEvento.price}</p>
+                                        <p class="card-text">Date: ${cardEvento.date}</p>
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>    
+    `;
--- assets/js/past.js
+++ assets/js/past.js
@@ -7,30 +7,27 @@ console.log(eventos);
 let eventCartas = [];
 
 function createEvents() {
-    
-    for (let event of eventos){
-        if (event.date < data.currentDate){
-            let cartaEv = `<div class="card mt-4" style="width: 30rem;" >
+  for (let event of eventos) {
+    if (event.date < data.currentDate) {
+      let cartaEv = `<div class="card mt-4" style="width: 30rem;" >
         <img src="${event.image}" class="card-img-top img-fit" alt="cinema">
         <div class="card-body">
             <h5 class="card-title">${event.name}</h5>
             <p class="card-text">${event.description}</p>
             <p>Price: $ ${event.price}</p>
-            <a href="./details.html" class="btn btn-primary">More info</a>
+            <a href="./details.html?id=${event._id}" class="btn btn-primary">More info</a>
         </div>
-    </div>`
-    eventCartas.push (cartaEv)}
-        
+    </div>`;
+      eventCartas.push(cartaEv);
     }
-
-    
+  }
 }
 
 console.log(eventCartas);
 
-function printEvents(){
-    let cartaEv = document.getElementById('cardEvents');
-    cartaEv.innerHTML = eventCartas.join('')
+function printEvents() {
+  let cartaEv = document.getElementById("cardEvents");
+  cartaEv.innerHTML = eventCartas.join("");
 }
 
 createEvents();
