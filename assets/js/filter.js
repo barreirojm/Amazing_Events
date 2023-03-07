2dd846cb5b06cf1358b170bc095a407e78c6721e
barreirojm <barreirojuanmanuel@gmail.com>
2023-03-07 11:47:46 -0300

    reestructuracion de los js

--- assets/js/filter.js
+++ assets/js/filter.js
@@ -1,17 +1,23 @@
-/* function captureData() {
-  let texto = document.getElementById("id_search").value.toLowerCase();
-
-  console.log(texto);
+function captureData() {
+  
+  let texto = document.getElementById("id_search").value;
   let checks = Array.from(
     document.querySelectorAll(".class_checks:checked")
   ).map((each) => each.value);
+  
   let filtro = data.events.filter((each) => {
-    let nameEventstoLower = each.name.toLowerCase();
+    let nameEventstoLower = each.name.toLowerCase()
     return (
       nameEventstoLower.includes(texto) &&
       (checks.length === 0 || checks.includes(each.category))
     );
   });
+
   console.log(filtro);
-  createEvents(filtro, "cardEvents");
-} */
+
+  if (filtro.length > 0) {
+    printTemplates("#cardEvents", filtro);
+  } else {
+    notFound("#cardEvents");
+  }
+}
