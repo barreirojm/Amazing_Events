2a0afe5de3c86b91a3c6730828826ffbe092a020
barreirojm <barreirojuanmanuel@gmail.com>
2023-03-07 21:02:02 -0300

    alert de contact form

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,3 +1,4 @@
+let dataForm = []
 
 function captureData() {
     let name = document.getElementById('name_form')
@@ -10,19 +11,38 @@ function captureData() {
         [message.name]: message.value
     }
     console.log(dataForm);
+    console.log(dataForm.name);
+
+    alert(`Hi there ${dataForm.name} , thanks for reaching out.\nWe've received your message and will get back to you as soon as possible at your email: ${dataForm.email}\nAmazing Events.-`);
+        
+    /* Swal.fire({
+        icon: 'success',
+        title: 'Datos ingresados:',
+        html: `Nombre: ${captureData()[dataForm.name]}<br>Correo electrónico: ${dataForm.email}`
+      }); */
+    
 }
+
+
 
 const handleForm = (event) => {
     event.preventDefault()
     captureData()
+    
     /* alert("Hi there, thanks for reaching out. We've received your message and will get back to you as soon as possible.") */
-    Swal.fire(
-        'Hi there! Thanks for reaching out.',
-        'We have received your message and will get back to you as soon as possible.',
-        'success'
-      )
+
+    
+
+    /* Swal.fire({
+        icon: 'success',
+        title: 'Datos ingresados:',
+        html: `Nombre: ${[dataForm.name]}<br>Correo electrónico: ${dataForm.email}`
+      }); */
+        
 }
 
 let buttonForm = document.getElementById('form-button')
 buttonForm.addEventListener('click', handleForm)
+
+
 
